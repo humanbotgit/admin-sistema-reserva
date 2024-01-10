87464079a544e904a6ff6fb8c7d4b43d98b124c5
humanbotgit <jarancibiasedano@gmail.com>
2024-01-10 16:52:39 -0500

    not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UsuariosComponent } from './interface/usuarios/usuarios.component';
 import { AsignaturasComponent } from './interface/asignaturas/asignaturas.component';
 import { ReservasComponent } from './interface/reservas/reservas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SidebarComponent,
     UsuariosComponent,
     AsignaturasComponent,
-    ReservasComponent
+    ReservasComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
